707b8eab74e42f8bc315ae33bda2188087b565bb
working computer <jihkie@users.noreply.github.com>
2017-03-22 16:49:08 -0700

    retyped basically the whole gear arm class

--- Mecanum Drive/src/org/usfirst/frc/team2555/robot/GearArm.java
+++ Mecanum Drive/src/org/usfirst/frc/team2555/robot/GearArm.java
@@ -1,5 +1,29 @@
 package org.usfirst.frc.team2555.robot;
 
-public class GearArm {
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
 
+public class GearArm {
+	public Solenoid gearGrabber;
+	public DoubleSolenoid gearArm;
+	
+	public GearArm(int armDownCh, int armUpCh, int gearGrabCh) {
+		gearGrabber = new Solenoid(gearGrabCh);
+		gearArm = new DoubleSolenoid(armDownCh, armUpCh);
+	}
+	
+	public void GearArmDown() {
+		gearArm.set(DoubleSolenoid.Value.kForward);
+	}
+	public void GearArmUp() {
+		gearArm.set(DoubleSolenoid.Value.kReverse);
+	}
+	public void GearGrab() {
+		gearGrabber.set(true);
+	}
+	public void GearRelease() {
+		gearGrabber.set(false);
+	}
+	
+	
 }
