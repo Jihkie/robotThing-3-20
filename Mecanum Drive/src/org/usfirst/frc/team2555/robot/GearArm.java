75a2e539e50bcf1c87629b898a70f785f45fe0ef
working computer <jihkie@users.noreply.github.com>
2017-03-22 17:15:29 -0700

    made the full grab function

--- Mecanum Drive/src/org/usfirst/frc/team2555/robot/GearArm.java
+++ Mecanum Drive/src/org/usfirst/frc/team2555/robot/GearArm.java
@@ -2,11 +2,12 @@ package org.usfirst.frc.team2555.robot;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
 
 public class GearArm {
 	public Solenoid gearGrabber;
 	public DoubleSolenoid gearArm;
-	
+	boolean gearIsGrabbed = false;
 	public GearArm(int armDownCh, int armUpCh, int gearGrabCh) {
 		gearGrabber = new Solenoid(gearGrabCh);
 		gearArm = new DoubleSolenoid(armDownCh, armUpCh);
@@ -25,5 +26,16 @@ public class GearArm {
 		gearGrabber.set(false);
 	}
 	
-	
+	public void FullGearGrab() {
+		if(!gearIsGrabbed) {
+			GearRelease();
+			Timer.delay(0.3);
+			GearArmDown();
+			Timer.delay(1.0);
+			GearGrab();
+			Timer.delay(0.25);
+			GearArmUp();
+			gearIsGrabbed = true;
+		}
+	}
 }
